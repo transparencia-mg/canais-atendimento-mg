3f1aec801a6de8f2f49d5f8f671369603d9ca0be
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-10-19 12:09:49 -0300

    Commit inicial

---
+++ canais-atendimento-mg.R
@@ -0,0 +1,27 @@
+library("tidytext"); library("dplyr"); library("stringi"); library("writexl")
+
+df_raw <- read.csv2("data/relatorio.csv", stringsAsFactors = FALSE)
+
+names(df_raw) <- janitor::make_clean_names(names(df_raw))
+
+df <- df_raw %>% 
+      select(protocolo, pergunta)
+
+df_tidy <- df %>% 
+          unnest_tokens(keyword, pergunta) %>% # extrair todas as palavras da pergunta
+          mutate(keyword = stri_trans_general(keyword, "latin-ascii")) # remover acentos
+
+df_tidy %>%
+  count(keyword, sort = TRUE)
+
+# remover palavras que nao auxiliam o processo de classificacao
+stopwords <- read.csv2("data/stopwords.txt", stringsAsFactors = FALSE)
+
+df_tidy <- anti_join(df_tidy, stopwords, by = c("keyword"))
+
+df_tidy %>%
+  count(keyword, sort = TRUE)
+
+# excel para gerar tabela dinamica que permite filtro por palavra chave
+left_join(df_tidy, df, by = "protocolo") %>% write_xlsx("data/keywords.xlsx")
+  
