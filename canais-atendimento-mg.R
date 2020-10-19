e77a059a4409e2bceccfd0e9f56917f579575f57
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-10-19 12:10:07 -0300

    Altera script para leitura de relatorio em xls

--- canais-atendimento-mg.R
+++ canais-atendimento-mg.R
@@ -1,6 +1,6 @@
-library("tidytext"); library("dplyr"); library("stringi"); library("writexl")
+library("tidytext"); library("dplyr"); library("stringi"); library("writexl"); library("readxl")
 
-df_raw <- read.csv2("data/relatorio.csv", stringsAsFactors = FALSE)
+df_raw <- read_excel("data/relatorio.xls")
 
 names(df_raw) <- janitor::make_clean_names(names(df_raw))
 
@@ -11,16 +11,14 @@ df_tidy <- df %>%
           unnest_tokens(keyword, pergunta) %>% # extrair todas as palavras da pergunta
           mutate(keyword = stri_trans_general(keyword, "latin-ascii")) # remover acentos
 
-df_tidy %>%
-  count(keyword, sort = TRUE)
-
 # remover palavras que nao auxiliam o processo de classificacao
 stopwords <- read.csv2("data/stopwords.txt", stringsAsFactors = FALSE)
 
 df_tidy <- anti_join(df_tidy, stopwords, by = c("keyword"))
 
 df_tidy %>%
-  count(keyword, sort = TRUE)
+  count(keyword, sort = TRUE) %>% 
+  mutate(part = n / sum(n))
 
 # excel para gerar tabela dinamica que permite filtro por palavra chave
 left_join(df_tidy, df, by = "protocolo") %>% write_xlsx("data/keywords.xlsx")
